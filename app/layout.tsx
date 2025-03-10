155512868a391c320261514ad17427fce5485242
Hayeon Baek <bhy0512@gmail.com>
2025-03-10 14:39:17 +0900

    Fix: Apply Provider only to the body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,24 +18,24 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <ReactQueryClientProvider>
-      <ThemeProvider>
-        <html lang='en'>
-          <head>
-            <link
-              rel='stylesheet'
-              href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
-              integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=='
-              crossOrigin='anonymous'
-              referrerPolicy='no-referrer'
-            />
-          </head>
-          <body className={poppins.className}>{children}</body>
-        </html>
-      </ThemeProvider>
-    </ReactQueryClientProvider>
+    <html lang='en'>
+      <head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
+          integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=='
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
+        />
+      </head>
+      <body className={poppins.className}>
+        <ReactQueryClientProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ReactQueryClientProvider>
+      </body>
+    </html>
   )
 }
